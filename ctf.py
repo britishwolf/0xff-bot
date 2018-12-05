353f3d7f1342322229d726e7961cf6db402a5c46
rstular <rok@stular.eu>
2018-12-06 00:41:53 +0100

    SHA256 and SHA1

--- ctf.py
+++ ctf.py
@@ -62,4 +62,27 @@ class CTF():
             await self.bot.say("An error occured!")
 
 
+    @commands.command(name="sha256",
+            description="Calculates SHA256 value of a string",
+            brief="SHA256 hash")
+    async def sha256(self, input_string : str):
+        try:
+            h = hashlib.sha256()
+            h.update(input_string.encode("utf-8"))
+            output = h.hexdigest()
+            await self.bot.say("```%s```" % output)
+        except:
+            await self.bot.say("An error occured!")
+
+    @commands.command(name="sha1",
+            description="Calculates SHA1 value of a string",
+            brief="SHA1 hash")
+    async def sha1(self, input_string : str):
+        try:
+            h = hashlib.sha1()
+            h.update(input_string.encode("utf-8"))
+            output = h.hexdigest()
+            await self.bot.say("```%s```" % output)
+        except:
+            await self.bot.say("An error occured!")
 
