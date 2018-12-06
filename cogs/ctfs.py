bf6b247b788cfb5e7dfa44e0e336fcde6ba68d2f
rstular <rok@stular.eu>
2018-12-06 03:26:27 +0100

    Bug fixes, styling changes

--- cogs/ctfs.py
+++ cogs/ctfs.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 
 import requests
-import json
+import time
 from dateutil.parser import parse
 
 class CTFs():
@@ -14,13 +14,20 @@ class CTFs():
         brief="List upcoming CTF competitions",
         aliases=["uc"])
     async def upcoming(self):
+        url_template = "https://ctftime.org/api/v1/events/?limit=15&start=%s&online=1&restrictions=0"
         headers = {"User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:63.0) Gecko/20100101 Firefox/63.0"}
-        r = requests.get("https://ctftime.org/api/v1/events/?limit=100&start=1544059804&online=1&restrictions=0", headers=headers).text
-        r = json.loads(r)
-        em = discord.Embed(title="Upcoming CTFs", type="rich", color=4622836)
+
+        r = requests.get(url_template % str(int(time.time())), headers=headers).json()
+
+        em = discord.Embed(title="**Upcoming CTFs**", description="CTF competitions in the near future (displaying max. 15 results)", type="rich", color=4622836)
+
+        title_template="%s [%s]"
+
         for ctf in r:
-            ctfStart = parse(ctf["start"]).strftime("%d-%m-%Y %H:%M")
-            em.add_field(name=ctf["title"], value=ctfStart, inline=False)
+            if ctf["location"] == "":
+                ctfStart = parse(ctf["start"]).strftime("%d-%m-%Y %H:%M")
+                em.add_field(name=title_template % (ctf["title"], ctf["url"]), value=ctfStart, inline=False)
+        em.set_footer(text="Generated on: " + str(int(time.time())))
         await self.bot.say(embed=em)
 
 
